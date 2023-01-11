87e6c17d92b83a97073641b297f5f85f8b1a98f7
MyUsername <my@email.com>
2023-01-11 14:48:02 +0100

    insert store js preset

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,12 +6,25 @@ export default {
 </script>
 
 <template lang="">
-   <div>
-      
+<div class="wrapper-bg">
+   <div class="card-container">
+      <h1>card</h1>
    </div>
+</div>
 </template>
 
 
-<style lang="scss">
-   
+<style lang="scss" scoped>
+   .wrapper-bg{
+      width: 100%;
+      height: 100vh;
+      background-color: #a6b5a3;
+
+      .card-container{
+         margin: 0 auto;
+         width: 150vh;
+         height: 90vh;
+         background-color: white;
+      }
+   }
 </style>
