6eaba356b1dfdd0806c4b76f493de28d2ac73043
MyUsername <my@email.com>
2023-01-12 20:25:00 +0100

    create laber and select

--- src/components/AppSelectCard.vue
+++ src/components/AppSelectCard.vue
@@ -1,14 +1,32 @@
 
 <script>
+
+import { store } from '../store.js'
+
 export default {
-   
+   name: "AppSelectCard",
+   data() {
+      return{
+         store,
+      }
+   },
+
+
 }
 </script>
 
 <template lang="">
-   <div>
-      
-   </div>
+
+   <label for="typeCard">Choose an archetype of card:</label>
+   <select name="typeCard" id="typeCard" v-model="store.selectArchetype" @change="$emit('getCard')">
+      <option value="Alien">Alien</option>
+      <option value="Laval">Laval</option>
+      <option value="Vylon">Vylon</option>
+      <option value="Inzektor">Inzektor</option>
+      <option value="Umi">Umi</option>
+      <option value="Gusto">Gusto</option>
+   </select>
+
 </template>
 
 
